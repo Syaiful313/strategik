e2849b759b5654c0e9acef0f029a000fc986c6bc
Syaiful313 <mthitz313@gmail.com>
2025-01-27 19:00:24 +0700

    feat : animation service detail

--- src/app/services/[slug]/page.tsx
+++ src/app/services/[slug]/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+import FadeInSection from "@/components/FadeInSection";
 import { getEntryService } from "@/lib/contentful";
 import { RICHTEXT_SERVICE } from "@/lib/richtext-service";
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
@@ -20,44 +22,55 @@ const ServiceDetail: FC<ServiceDetailProps> = async ({ params }) => {
         >
           <div className="container mx-auto py-8 md:py-12">
             <div className="mx-auto max-w-6xl px-4 sm:px-6 md:mb-12 md:px-8">
-              <h1 className="mb-4 text-4xl font-bold sm:text-5xl md:mb-6 md:text-6xl lg:text-7xl xl:text-8xl">
-                {service.title}
-              </h1>
-              <p className="max-w-6xl text-lg sm:text-xl md:text-2xl">
-                {service.desciption}
-              </p>
+              <FadeInSection delay={200}>
+                <h1 className="mb-4 text-4xl font-bold sm:text-5xl md:mb-6 md:text-6xl lg:text-7xl xl:text-8xl">
+                  {service.title}
+                </h1>
+              </FadeInSection>
+              <FadeInSection delay={400}>
+                <p className="max-w-6xl text-lg sm:text-xl md:text-2xl">
+                  {service.desciption}
+                </p>
+              </FadeInSection>
             </div>
           </div>
 
           <div className="container mx-auto pb-5">
-            <div className="mb-8 flex items-center justify-center md:mb-12">
-              <div className="relative aspect-[1090/600] w-full max-w-[1090px]">
+            <FadeInSection delay={400}>
+              <div className="mb-8 flex items-center justify-center md:mb-12">
+                <div className="relative aspect-[1090/600] w-full max-w-[1090px]">
+                  <Image
+                    src={service.thumbnail}
+                    alt="thumbnail"
+                    fill
+                    className="rounded-lg object-cover object-center"
+                    priority
+                  />
+                </div>
+              </div>
+            </FadeInSection>
+
+            <div className="mx-auto mb-8 max-w-4xl px-4 sm:px-6 md:mx-12 md:mb-12 md:px-8">
+              <FadeInSection delay={400}>
+                {documentToReactComponents(service.content, RICHTEXT_SERVICE)}
+              </FadeInSection>
+            </div>
+            <FadeInSection delay={400}>
+              <div className="relative mx-auto mb-8 aspect-[600/350] w-full max-w-[600px] md:mx-12 md:mb-12">
                 <Image
-                  src={service.thumbnail}
+                  src={service.thumbail2}
                   alt="thumbnail"
                   fill
                   className="rounded-lg object-cover object-center"
                   priority
                 />
               </div>
-            </div>
+            </FadeInSection>
 
-            <div className="mx-auto mb-8 max-w-4xl px-4 sm:px-6 md:mx-12 md:mb-12 md:px-8">
-              {documentToReactComponents(service.content, RICHTEXT_SERVICE)}
-            </div>
-
-            <div className="relative mx-auto mb-8 aspect-[600/350] w-full max-w-[600px] md:mx-12 md:mb-12">
-              <Image
-                src={service.thumbail2}
-                alt="thumbnail"
-                fill
-                className="rounded-lg object-cover object-center"
-                priority
-              />
-            </div>
-
-            <div className="mx-auto max-w-4xl px-4 md:pb-20 sm:px-6 md:mx-12 md:px-8">
-              {documentToReactComponents(service.content2, RICHTEXT_SERVICE)}
+            <div className="mx-auto max-w-4xl px-4 sm:px-6 md:mx-12 md:px-8 md:pb-20">
+              <FadeInSection delay={400}>
+                {documentToReactComponents(service.content2, RICHTEXT_SERVICE)}
+              </FadeInSection>
             </div>
           </div>
         </section>
